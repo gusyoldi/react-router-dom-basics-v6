2d4c4f15cf476ec4df42eb3419b362603f5a569b
Elroli09 <yoldi.gustavo@gmail.com>
2023-01-31 22:43:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Link, Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { Landing, Home, Dashboard, Analytics, Admin } from "./pages/components";
+
+function Navigation() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Landing</Link>
+				</li>
+				<li>
+					<Link to="/home">Home</Link>
+				</li>
+				<li>
+					<Link to="/dashboard">Dashboard</Link>
+				</li>
+				<li>
+					<Link to="/analytics">Analytics</Link>
+				</li>
+				<li>
+					<Link to="/admin">Admin</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+function ProtectedRoute({ isAllowed, children, redirectTo = "/" }) {
+	if (!isAllowed) {
+		return <Navigate to={redirectTo} />;
+	} else {
+		return children ? children : <Outlet />;
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [user, setUser] = useState(null);
+
+	const login = () => {
+		setUser({
+			id: 1,
+			name: "John",
+			permissions: ["analyze"],
+			roles: ['admin']
+		});
+	};
+
+	const logout = () => {
+		setUser(null);
+	};
+
+	return (
+		<BrowserRouter>
+			<Navigation />
+
+			{user ? (
+				<>
+					<button onClick={logout}>Logout</button>
+					<span> Usuario logueado</span>
+				</>
+			) : (
+				<>
+					<button onClick={login}>Login</button>
+					<span> Usuario deslogueado</span>
+				</>
+			)}
+
+			<Routes>
+				<Route index element={<Landing />} />
+				<Route element={<ProtectedRoute isAllowed={!!user} />}>
+					<Route path="/home" element={<Home />} />
+					<Route path="/dashboard" element={<Dashboard />} />
+				</Route>
+					<Route path="/analytics" element={
+				<ProtectedRoute 
+					isAllowed={!!user && user.permissions.includes("analyze")} 
+					redirectTo={'/home'}>
+
+					<Analytics />
+				</ProtectedRoute>
+					} />
+				<Route path="/admin" element={
+					<ProtectedRoute 
+					isAllowed={!!user && user.roles.includes("admin")} 
+					redirectTo={'/home'}>
+
+					<Admin />
+				</ProtectedRoute>
+				} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/components.jsx
@@ -0,0 +1,18 @@
+export function Landing() {
+	return <h1>Landing Page (Public)</h1>;
+}
+export function Home() {
+	return <h1>Home Page (Private)</h1>;
+}
+
+export function Dashboard() {
+	return <h1>Dashboard Page (Private)</h1>;
+}
+
+export function Analytics() {
+	return <h1>Analytics (Private, permission: 'analyze') </h1>;
+}
+export function Admin() {
+	return <h1>Admin (Private, permission: 'admin')</h1>;
+}
+
